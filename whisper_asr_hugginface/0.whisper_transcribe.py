1e535fc17323562dad31e9e38daf4629e1107a85
selina <seimy6681@gmail.com>
2025-05-19 15:18:10 +0900

    added uar calculation in transcribe

--- whisper_asr_hugginface/0.whisper_transcribe.py
+++ whisper_asr_hugginface/0.whisper_transcribe.py
@@ -9,7 +9,7 @@ import jiwer
 from sklearn.metrics import recall_score
 from transformers import WhisperForConditionalGeneration
 from transformers import WhisperProcessor
-
+from sklearn.metrics import recall_score
 
 # 테스트 csv 파일 로딩
 df = pd.read_csv('/home/selinawisco/hdd/korean_asr/r08.1_test.csv')
@@ -70,5 +70,63 @@ def calculate_row_cer(row):
 df['CER'] = df.apply(lambda x: calculate_row_cer(x), axis=1)
 jiwer_avg_cer = df['CER'].mean()
 print(f'{jiwer_avg_cer}')
+
+
+TARGET_COL_NAME = 'target_text_jamo' #타겟단어 칼럼
+PRED_COL_NAME = f'asr_{MODE}_transcription'
+
+# # 전사 데이터 로딩 ------------------------------------------------------------------------
+# test = f'/home/selinawisco/finetuning_baseline_43_1'
+# df = pd.read_csv(test)
+
+# 단어 추출 (whitespace 제거)
+df[TARGET_COL_NAME] = df[TARGET_COL_NAME].str.strip() # label 타겟 ground truth
+df[PRED_COL_NAME] = df[PRED_COL_NAME].str.strip()
+
+true_positives = 0
+true_negatives = 0
+false_positives = 0
+false_negatives = 0
+
+fp_errors = {}
+fn_errors = {}
+tp = []
+
+fn_rows = []
+
+for idx,(_, row) in enumerate(df.iterrows()):
+    target_jamo = row[TARGET_COL_NAME]
+    # asr = row['asr_transcription']
+    asr = row[PRED_COL_NAME]
+    new_label = row['new_label']
+    
+    if new_label == 1: # 레이블 상 단어 발음이 틀렸을 시
+        if target_jamo != asr: # asr이 타겟단어와 다르기만 하면 True Positive 장애진단 (정답)
+            true_positives += 1
+            tp.append(row)
+            df.at[idx, 'pred_by_ASR'] = 1
+            
+        else:
+            false_negatives += 1 # asr과 타겟단어가 일치하면 false negative 정상진단 (오류)   
+            dict_key = row['target_text'] + "_" + str(idx)         
+            fn_errors[dict_key] = (row['human_text'])
+            fn_rows.append(row)
+            df.at[idx, 'pred_by_ASR'] = 0
+            
+            
+    elif new_label == 0: # 레이블 상 단어 발음이 정상일 시
+        if target_jamo == asr: # asr과 타겟단어가 같으면 정상진단 (정답)
+            true_negatives += 1
+            df.at[idx, 'pred_by_ASR'] = 0
+            
+        else:
+            false_positives += 1 # asr 과 타겟단어가 다르면 장애진단 (오류)
+            dict_key_2 = row['target_text'] + "_" + str(idx)
+            fp_errors[dict_key_2] = hangul_jamo.compose(row['asr_human_transcription'])
+            df.at[idx, 'pred_by_ASR'] = 1
+   
+
+uar = recall_score(df[new_label], df['asr_human_transcription'], average='macro')
+print(f'{uar=}')
 
 df.to_csv(f'/home/selinawisco/selina_main/asr/whisper-small-hi-45-eval.csv', index=False)
