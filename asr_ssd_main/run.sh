5216fb121fe4421af903f7717750c3925e371007
selina <seimy6681@gmail.com>
2025-06-02 15:30:50 +0900

    main.py and others added

---
+++ asr_ssd_main/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for fold in {1..1}
+do 
+    seed=42
+    echo "running for fold $fold with seed $seed"
+
+    CUDA_VISIBLE_DEVICES=0 CUDA_LAUNCH_BLOCKING=0 python main.py \
+    --epochs 30 \
+    --batch_size 8 \
+    --target 'human_text_jamo' \
+    --asr_mode 'human' \
+    --train_filename "nas_data/five_fold_datasets/test_fold_${fold}_train.csv" \
+    --test_filename "nas_data/five_fold_datasets/test_fold_${fold}.csv" \
+    --num_runs 1 \
+    --seed $seed 
+done 
+# --multitask_alpha 0.15 \
+# --attention 'single' \
+# --loss_feature 'binary_label' \
+# --loss_feature 'age_label' \
+# --embedding \
+# --age_loss_weighting
+# --reverse_alpha 0.5 \
+# --train_filename 'tacotron_tts_0421_baseline_44_fn_sigma1.csv' \
---
+++ asr_ssd_main/train_asr.py
@@ -0,0 +1,65 @@
+from transformers import Trainer
+from CustomTrainer import CustomTrainer
+from transformers import TrainingArguments
+import torch
+import warnings
+import os
+
+def train_asr(model, data_collator, processor, tokenizer,feature_extractor, train, test, compute_metrics, config, curr_seed):
+    if config.loss_feature is None:
+        config.loss_feature = 'baseline'
+    training_args = TrainingArguments(
+            seed=curr_seed,
+            # output_dir=f"/data/selinawisco/spring/wav2vec2-word-level-asr_{config.train_filename}_{config.loss_feature}_local_attention_only_{curr_seed}", # 모델 저장 경로 
+            # output_dir=f"/data/selinawisco/spring/0508_{config.loss_feature}_embedding_{curr_seed}", # 모델 저장 경로 
+            output_dir=f"/data/selinawisco/journal_result/wav2vec2-word-level-asr-baseline_{config.train_filename}_{curr_seed}",
+            group_by_length=True,
+            # per_device_train_batch_size=1,
+            per_device_train_batch_size=8,
+            gradient_accumulation_steps=2,
+            gradient_checkpointing=False, #True,
+            evaluation_strategy="steps",
+            num_train_epochs=config.epochs,
+            fp16=True,
+            save_steps=100,
+            eval_steps=1000,
+            # eval_steps=20,
+            logging_steps=10,
+            learning_rate = 3e-4,
+            warmup_steps=500,
+            save_total_limit=2,
+            # push_to_hub = True,
+        )
+    
+    print(f'saving model in {training_args.output_dir=}')
+
+    trainer = CustomTrainer(
+        config=config,
+        model=model,
+        data_collator=data_collator,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=train,
+        eval_dataset=test,
+        tokenizer=processor.feature_extractor,
+    )
+
+    torch.cuda.empty_cache()
+    trainer.train()
+    # trainer.train(resume_from_checkpoint='//data/selinawisco/asr_finetuned/joint_0.15_42/checkpoint-24100')
+    
+    tokenizer.save_pretrained(training_args.output_dir) 
+    feature_extractor.save_pretrained(training_args.output_dir)
+    model.save_pretrained(training_args.output_dir)
+    
+    # temporary fix : unknow error in evaluate
+    try:
+        trainer.evaluate(model)
+    except Exception as e:
+        warnings.warn("evaluation loop crashed, skipping . . .")
+        return None
+        
+        
+    # test(model, training_args.output_dir, processor, curr_seed)
+    
+
